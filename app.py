f3bee9996481de26cf8ef7a8b89fb6875622e534
mspowercodes <dpower@isbos.org>
2026-08-03 13:36:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,14 @@ def caesar_cipher_simple(message):
     encrypted_message = ""
     
     for char in message.lower():
-        # FIXED: Changed 'plain' to 'plaintext'
         if char in plaintext:
             position_plain = plaintext.find(char)
             encrypted_message += ciphertext[position_plain]
         else:
-            # FIXED: Changed 'text_cipher' to 'encrypted_message'
             encrypted_message += char
             
-    # FIXED: Changed 'text_cipher' to 'encrypted_message'
     return encrypted_message
 
-# Streamlit Interface
 st.title("Simple Caesar Cipher")
 
 user_message = st.text_input("Enter text to encrypt:", value="hello")
