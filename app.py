15e59c5dc9c9b95b88eb0aa38a238bd0def8e926
mspowercodes <dpower@isbos.org>
2026-08-05 09:44:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ def caesar_shift3(message):
     return message.translate(table)
 
 # --- STREAMLIT INTERFACE ---
-st.title("The Cryptoclub - Chapter 1")
-st.write("This app encrypts lowercase text using a shift of 3.")
+st.title("Cryptocoding in Python Lesson 1")
+st.write("This app encrypts lowercase text using a +3 Caesar cipher.")
 
 # Captures what the student types into the box
-user_text = st.text_input("Enter lowercase plaintext:", value="hello world")
+user_text = st.text_input("Enter a message:", value="hello world")
 
 # Runs the simpler string function
 result = caesar_shift3(user_text)
 
 # Displays the final secret message
-st.write("**Ciphertext:**")
+st.write("Encrypted message:")
 st.code(result)
